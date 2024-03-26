6db1f81ff9ddcc7bf89ac983d0435f60361f895b
Александр Александр <stripalol@inbox.ru>
2024-03-26 16:47:14 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApplication57/ConsoleApplication57.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+using namespace std;
+class Power
+{
+	int cash;
+public:
+	Power(int cash)
+	{
+		this->cash = cash * cash;
+		cout << this->cash;
+	}
+
+	Power(int cash, int stepen)
+	{
+		for (int i = 0; i < stepen; i++)
+		{
+			this->cash = cash *= cash;
+		}
+		cout << this->cash;
+	}
+
+	Power operator+(const Power& power)
+	{
+		return Power{ cash + power.cash };
+	}
+
+	Power operator-(const Power& power)
+	{
+		return Power{ cash - power.cash };
+	}
+
+	Power operator*(const Power& power)
+	{
+		return Power{ cash * power.cash };
+	}
+
+	Power operator/(const Power& power)
+	{
+		return Power{ cash / power.cash };
+	}
+	Power operator+=(const Power& power)
+	{
+		return Power{ cash += power.cash };
+	}
+	void display()
+	{
+		cout << cash;
+	}
+};
+int main()
+{
+	Power power(2);
+	power.display();
+	cout << '\n';
+	Power power2(2, 4);
+	power2.display();
+	power + power2;
+	cout << '\n';
+	power.display();
+}
